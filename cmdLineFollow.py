914ceb0719120a40615d48530485cb13ebde53bf
PhilipHoltzman <phil.holtzman@gmail.com>
2016-03-13 23:11:14 -0600

    added the cmd line follow script

---
+++ cmdLineFollow.py
@@ -0,0 +1,27 @@
+import tweepy
+import sys
+from keys import tateKey #keys
+
+# boilerplate for authentication
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+
+
+# import keys from keyfile and store in the cfg var
+cfg = tateKey
+api = get_api(cfg)
+
+# assign command line arg as 'a'
+# a is the USERNAME
+a = ' '.join(sys.argv[1:])
+
+
+# check if there is a user in command
+if len(a) > 1:
+  # follow the user
+  api.create_friendship(a)
+else:
+    print('Provide a string!')
